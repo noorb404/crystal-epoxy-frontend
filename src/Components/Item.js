73761f3ab307ec983a76e5268f3204a5ee48351f
noor <noor.bakrieh@yahoo.com>
2023-05-04 08:51:16 +0300

    feature added

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -28,17 +28,34 @@ export default function Item(props) {
             <div className="products__category">
                 <div className="products__grid">
                     {/* Table Items */}
-                    {products.map((product, index) => (
-                        <a key={product.id}  className="products__item custom-a" href={`/product/${product.id}`}>
-                            <Image className='products__image' cloudName='ddlf8wxvm' publicId={product.image}/>
-                            <div className="products__item-info">
-                            <p className="products__item-description">{product.name}</p>
-                                <p className="products__item-description-price">{product.price}₪</p> 
-                                 <p className="products__item-price">{product.price}₪</p>
-                            </div>
-                        </a>
-                    ))}
+                    {products.length === 0 ?
+                        <div className="no-products">Coming soon!</div>
+                        :
+                        products.map((product, index) => (
+                            <a key={product.id}  className="products__item custom-a" href={`/product/${product.id}`}>
+                                <Image className='products__image' cloudName='ddlf8wxvm' publicId={product.image}/>
+                                <div className="products__item-info">
+                                    <p className="products__item-description">{product.name}</p>
+                                    <p className="products__item-description-price">{product.price}₪</p> 
+                                    <p className="products__item-price">{product.price}₪</p>
+                                </div>
+                            </a>
+                        ))
+                    }
                 </div>
+                {products.length === 0 &&
+                    <style jsx>{`
+                        .no-products {
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            font-size: 24px;
+                            font-weight: bold;
+                            color: #999;
+                            margin-top: 50px;
+                        }
+                    `}</style>
+                }
             </div>
         </section>
     )
